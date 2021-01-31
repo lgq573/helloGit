4ac889f9716de181b8329839d7eaf125c4957ae7
lgq573 <78295536+lgq573@users.noreply.github.com>
2021-01-31 17:06:54 +0800

    Update hellogit.java

--- untitled4/src/main/java/com/atguigu/git/hellogit.java
+++ untitled4/src/main/java/com/atguigu/git/hellogit.java
@@ -7,6 +7,6 @@ public class hellogit {
         System.out.println("bubhbhbhbhbhh");
         System.out.println("分支");
         System.out.println("主干添加");
-        System.out.println("分支添加");
+        System.out.println("在线更新");
     }
 }
